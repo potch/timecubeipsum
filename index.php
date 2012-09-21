b94f0f08b1a5e6a0efc6d7ad263b9d9a673489d6
Matt Claypotch <thepotch@gmail.com>
2012-09-21 14:01:00 -0700

    TRUTH IS VERIFIED COMMIT

--- index.php
+++ index.php
@@ -3,11 +3,21 @@
   <head>
     <meta charset="utf-8">
     <title>Time Cube Ipsum</title>
+    <style>
+        body {
+            background: url(grid.gif);
+        }
+    </style>
   </head>
   <body>
-    <?
+    <h1>TIME CUBE IPSUM</h1>
+    <?php
         $num = $_SERVER['QUERY_STRING'];
-        echo $num;
+        $screeds = file_get_contents('tcipsum.txt').split('\n');
+        $nscreeds = count($screeds);
+        for ($i = 0; $i < $num; $i++) {
+            echo '<p>' . $screeds[rand(0,$nscreeds)] . '</p>';
+        }
     ?>
   </body>
 </html>
